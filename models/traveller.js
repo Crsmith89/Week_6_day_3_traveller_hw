70739e4aa0e8ff0bcb7cf54accc29173b45dd519
Craig Smith <craig_ag_smith@outlook.com>
2021-07-21 17:51:34 +0100

    stuck last one

--- models/traveller.js
+++ models/traveller.js
@@ -17,20 +17,32 @@ Traveller.prototype.getJourneyEndLocations = function () {
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter((journey) => {
+    if (journey.transport === transport) {
+      return journey;
+    };
+  });
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter((journey) => {
+    if (journey.distance > minDistance) {
+      return journey;
+    };
+  });
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  const total = this.journeys.reduce((runningTotal, journey) => {
+    return runningTotal + journey.distance;
+  }, 0);
+  return total;
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
-};
+  return this.journeys.map(journey => {
+    return journey.transport;
+  })
 
 
 module.exports = Traveller;
